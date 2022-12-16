49d0a8efbd29afefb95c9fd4853436bf54d9555a
Paolo Guray <froiodpaolo@gmail.com>
2022-12-17 01:09:35 +0800

    shortly website

---
+++ js/script.js
@@ -0,0 +1,45 @@
+
+const input = document.getElementById('link-input')
+const linkForm = document.getElementById('link-form')
+const errMsg = document.getElementById('err-msg')
+const menuBtn = document.getElementById('menu-btn')
+const menuItem = document.getElementById('menu')
+
+
+menuBtn.addEventListener('click', () => {
+  menuBtn.classList.toggle('open')
+  menuItem.classList.toggle('hidden')
+  menuItem.classList.toggle('flex')
+})
+
+linkForm.addEventListener('submit', formSubmit)
+
+// Validate a URL
+function validURL(str) {
+  var pattern = new RegExp(
+    '^(https?:\\/\\/)?' + // protocol
+      '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' +
+      '((\\d{1,3}\\.){3}\\d{1,3}))' +
+      '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' +
+      '(\\?[;&a-z\\d%_.~+=-]*)?' +
+      '(\\#[-a-z\\d_]*)?$',
+    'i'
+  )
+  return !!pattern.test(str)
+}
+
+function formSubmit(e) {
+  e.preventDefault()
+
+  if (input.value === ''){
+    errMsg.innerHTML = 'Please enter something'
+    input.classList.add('border-red')
+  } else if(!validURL(input.value)) {
+    errMsg.innerHTML = 'Please enter a valid url'
+    input.classList.add('border-red')
+  } else {
+    errMsg.innerHTML = ''
+    input.classList.remove('border-red')
+    alert('Success')
+  }
+}
